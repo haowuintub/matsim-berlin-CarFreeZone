89bbf4ef76fbd3654a7f9e59a42e30fde7acbbad
Hao Wu <wu.hao@tum.de>
2023-08-24 18:38:55 +0200

    new class

---
+++ src/main/java/org/matsim/run/carFreeZone/emission/MultipleZoneNoiseAnalysisRunner.java
@@ -0,0 +1,41 @@
+package org.matsim.run.carFreeZone.emission;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class MultipleZoneNoiseAnalysisRunner {
+
+    public static void main(String[] args) throws IOException {
+        // Path to the CSV file containing the postal codes (plz)
+        String csvFile = "/home/tumtse/Documents/haowu/DRZ/events_Biao/shapefiles/noise-analysis-zones/noise-analysis-zones_plz.csv";
+
+        try (CSVParser parser = new CSVParser(new FileReader(csvFile), CSVFormat.DEFAULT.withHeader())) {
+            // Iterate through the rows of the CSV file
+            for (var record : parser) {
+                // Retrieve the postal code (plz) from the current row
+                String plz = record.get("plz");
+
+                // Prepare the arguments for calling GenerateSingleAirPollutionValueWithinShape
+                String outputFile = "/home/tumtse/Documents/haowu/DRZ/events_Biao/output-berlin-v5.5-1pct-policyCase3_200_simple/emission/zonal-noise-analysis/berlin-v5.5-1pct.emission.events.offline.SingleAirPollutionValue_" + plz + ".csv";
+                List<String> analysisArgs = Arrays.asList(
+                        "-e", "/home/tumtse/Documents/haowu/DRZ/events_Biao/output-berlin-v5.5-1pct-policyCase3_200_simple/berlin-v5.5-1pct.emission.events.offline.xml.gz",
+                        "-o", outputFile,
+                        "-c", "/home/tumtse/Documents/haowu/DRZ/events_Biao/shapefiles/noise-analysis-zones/noise-analysis-zones.shp",
+                        "-p", plz,
+                        "-n", "scenarios/berlin-v5.5-1pct/DRZ/input/network-modified-carInternal_Plan3.xml.gz"
+                );
+
+                // Convert the arguments list to an array
+                String[] analysisArgsArray = analysisArgs.toArray(new String[0]);
+
+                // Call the GenerateSingleAirPollutionValueWithinShape class with the prepared arguments
+                GenerateSingleAirPollutionValueWithinShape.main(analysisArgsArray);
+            }
+        }
+    }
+}
